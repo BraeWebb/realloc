d231cacfcb90bd4eda959b5de21b6e84649b7c8d
LiamGraham <liam.david.graham@gmail.com>
2018-08-19 01:50:14 +1000

    Add temporary urls

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
 
+@app.route('/dashboard')
+def database():
+    return render_template("dashboard.html")
 
 @app.route('/coordinator')
 def coordinator():
