083046026732e54d20da8f5f9a26f8d6aa3aa432
BraeWebb <email@braewebb.com>
2018-08-19 00:07:02 +1000

    Fixed Template Path

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 
 @app.route('/api/user', methods=['GET'])
